54fed8c6ac4aaca501c2ab706b04573737aa43d2
JungKata <97729333+JungKata@users.noreply.github.com>
2022-09-22 08:29:58 +0200

    Festmenyek: -konstruktorok letrehozása

--- src/hu/petrik/festmenyekOOP/Festmenyek.java
+++ src/hu/petrik/festmenyekOOP/Festmenyek.java
@@ -1,2 +1,22 @@
-package hu.petrik.festmenyekOOP;public class Festmenyek {
+package hu.petrik.festmenyekOOP;
+
+import java.time.LocalDate;
+
+public class Festmenyek {
+private String cim;
+private String festo;
+private String stilus;
+private int licitekSzama;
+private int legmagasabbLicit;
+private LocalDate legutolsoLicitIdeje;
+private boolean elkelt;
+
+    public Festmenyek(String cim, String festo, String stilus, int licitekSzama, int legmagasabbLicit, LocalDate legutolsoLicitIdeje, boolean elkelt) {
+        this.cim = cim;
+        this.festo = festo;
+        this.stilus = stilus;
+
+    }
+
+
 }
